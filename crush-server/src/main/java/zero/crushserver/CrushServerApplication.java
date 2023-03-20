e9022c135a9e651f55a01b9521b5aefc8e4723f8
seungy0 <dltmdrb98@gmail.com>
2023-03-20 23:44:28 +0900

    Init spring boot

---
+++ crush-server/src/main/java/zero/crushserver/CrushServerApplication.java
@@ -0,0 +1,13 @@
+package zero.crushserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrushServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrushServerApplication.class, args);
+	}
+
+}
---
+++ crush-server/src/test/java/zero/crushserver/CrushServerApplicationTests.java
@@ -0,0 +1,13 @@
+package zero.crushserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrushServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
