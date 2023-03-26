bda89874609063d824ae36e0748a840c4793f5c9
seungy0 <dltmdrb98@gmail.com>
2023-03-26 21:14:34 +0900

    fix: Fix chatServiceTest for @Value
    @Value가 동작하도록 스프링 빈의 서비스 인스턴스를 사용하도록 수정함.
    
    참고: #13

--- crush-server/src/test/java/zero/crushserver/service/ChatServiceTest.java
+++ crush-server/src/test/java/zero/crushserver/service/ChatServiceTest.java
@@ -2,6 +2,8 @@ package zero.crushserver.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpEntity;
 import zero.crushserver.domain.ChatGptMessage;
 import zero.crushserver.domain.ChatGptRequest;
@@ -9,14 +11,17 @@ import zero.crushserver.domain.ChatGptResponse;
 
 import java.util.List;
 
+@SpringBootTest
 class ChatServiceTest {
 
+    @Autowired
+    ChatService chatService;
     @Test
     void buildHttpEntity() {
     }
 
     @Test
-    void getResponse() throws JsonProcessingException {
+    void getResponse() {
         ChatGptRequest chatGptRequest = new ChatGptRequest();
         chatGptRequest.setModel("gpt-3.5-turbo");
         ChatGptMessage chatGptMessage = new ChatGptMessage();
@@ -27,7 +32,6 @@ class ChatServiceTest {
         chatGptRequest.setMaxTokens(10);
         chatGptRequest.setTemperature(0.5);
         chatGptRequest.setTopP(1.0);
-        ChatService chatService = new ChatService();
         HttpEntity<ChatGptRequest> chatGptRequestHttpEntity = chatService.buildHttpEntity(chatGptRequest);
         ChatGptResponse response = chatService.getResponse(chatGptRequestHttpEntity);
         System.out.println(response.getChoices().get(0).getMessage().getContent());
